0aac704bac1e7008ad1a5a5e2c4d006b115cca5b
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-03-10 17:59:43 -0400

    Make simple facial reduction test work

--- gcs_facial_reduction/test_spp.py
+++ gcs_facial_reduction/test_spp.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import numpy.typing as npt
+import sympy
+from networkx.classes import DiGraph
 from pydrake.math import eq, ge, le
 from pydrake.solvers import MathematicalProgram, Solve
 
@@ -82,20 +84,85 @@ def _formulate_spp_problem(
     return path
 
 
-# Create a graph
-G = nx.DiGraph()
-
-# Add edges to the graph
-G.add_edge(0, 1)
-G.add_edge(1, 4)
-G.add_edge(4, 3)
-G.add_edge(0, 2)
-G.add_edge(2, 3)
-
-source = 0
-target = 3
+def e_i(i: int, dims: int) -> npt.NDArray:
+    """
+    Return the i-th unit vector.
+    """
+    e = np.zeros((dims,))
+    e[i] = 1
+    return e
 
 
-# draw_graph(G)
-path = _formulate_spp_problem(G, source, target)
-draw_path_in_graph(G, path)
+def _solve_facial_reduction_auxiliary_prob(A: npt.NDArray, b: npt.NDArray):
+    m, N = A.shape
+    prog = MathematicalProgram()
+    y = prog.NewContinuousVariables(m, "y")
+
+    x_hat = np.ones((N,))
+
+    prog.AddLinearConstraint(ge(A.T @ y, 0))
+    prog.AddLinearConstraint(b.T @ y == 0)
+    prog.AddLinearConstraint(x_hat.T @ A.T @ y == 1)
+
+    result = Solve(prog)
+
+    if result.is_success():
+        y_sol = result.GetSolution(y)
+        z = A.T @ y_sol
+        # x must be zero where z is nonzero
+        x_zero_idxs = np.where(~np.isclose(z, 0))[0].tolist()
+        return x_zero_idxs
+    else:
+        breakpoint()
+        raise NotImplementedError("Not yet implemented")
+        breakpoint()
+
+
+def facial_reduction_test(G: nx.DiGraph, source: int, target: int) -> None:
+    vertices = list(G.nodes())
+    edges = list(G.edges())
+    N = len(vertices)
+
+    A = nx.incidence_matrix(G, oriented=True).toarray()
+    b = _construct_b(source, target, N)
+
+    x_zero_idxs = _solve_facial_reduction_auxiliary_prob(A, b)
+    breakpoint()
+
+    # find linearly independent rows
+    sym_mat = sympy.Matrix(A)
+    inds = sym_mat.rref()[1]
+
+
+# TODO(bernhardpg): This can be turned into a unit test
+
+
+def test_problem() -> None:
+    A = np.array([[1, 1, 1, 1, 0], [1, -1, -1, 0, 1]])
+    b = np.array([1, -1])
+    x_zero_idxs = _solve_facial_reduction_auxiliary_prob(A, b)
+    assert x_zero_idxs == [0, 3, 4]
+
+
+def test_graph():
+    # Create a graph
+    G = nx.DiGraph()
+
+    # Add edges to the graph
+    G.add_edge(0, 1)
+    G.add_edge(1, 0)
+    G.add_edge(1, 4)
+    G.add_edge(4, 3)
+    G.add_edge(0, 2)
+    G.add_edge(2, 3)
+
+    source = 0
+    target = 3
+
+    # draw_graph(G)
+    facial_reduction(G, source, target)
+    path = _formulate_spp_problem(G, source, target)
+    draw_path_in_graph(G, path)
+
+
+test_problem()
